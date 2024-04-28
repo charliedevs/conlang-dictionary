e110c8e0413d8f209f1799a66360fbc3edaf8e01
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-04-27 22:16:03 -0600

    Add user info to analytics and use reverse proxy

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const coreConfig = {
   images: {
     remotePatterns: [{ hostname: "utfs.io" }],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 import { withSentryConfig } from "@sentry/nextjs";
--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -1,13 +1,39 @@
 "use client";
 
+import { useAuth, useUser } from "@clerk/nextjs";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: "https://us.i.posthog.com", // or 'https://eu.i.posthog.com' if your PostHog is hosted in Europe
   });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  return (
+    <PostHogProvider client={posthog}>
+      <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+    </PostHogProvider>
+  );
+}
+
+function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
+  const { isSignedIn } = useAuth();
+  const userInfo = useUser();
+
+  useEffect(() => {
+    if (userInfo.user) {
+      posthog.identify(userInfo.user.id, {
+        email: userInfo.user.primaryEmailAddress,
+        name: userInfo.user.fullName,
+        username: userInfo.user.username,
+      });
+    } else if (!isSignedIn) {
+      posthog.reset();
+    }
+  }, [isSignedIn, userInfo]);
+
+  return children;
 }
