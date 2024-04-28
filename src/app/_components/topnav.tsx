a7251a659ef255f23f12125ef85542df9cab1e0d
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-04-27 20:01:26 -0600

    Use custom upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,33 +1,19 @@
-"use client";
-
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { UploadButton } from "~/app/_components/upload-button";
 
 export function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4">
-      <a href="/" className="text-xl font-bold">
+      <a href="/" className="text-xl font-bold hover:text-white">
         Conlang Dictionary
       </a>
       <div className="flex items-center gap-4">
-        <a href="/">Gallery</a>
-        <div className="flex flex-row items-center gap-2">
+        <div className="flex flex-row items-center gap-4">
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
-            <UploadButton
-              endpoint="imageUploader"
-              onClientUploadComplete={() => {
-                router.refresh();
-              }}
-              onUploadError={(error) => {
-                alert(error.message);
-              }}
-            />
+            <UploadButton />
             <UserButton />
           </SignedIn>
         </div>
