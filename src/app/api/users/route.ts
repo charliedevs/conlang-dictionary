3196ab481a3c507455f700ce5658dbeba4b1c68a
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-04-30 21:43:50 -0600

    Add query to get userList

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,49 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { z } from "zod";
+
+export const getUsersSchema = z.object({
+  userId: z.array(z.string()).optional(),
+  username: z.array(z.string()).optional(),
+  emailAddress: z.array(z.string()).optional(),
+});
+
+export type User = {
+  id: string;
+  name: string;
+  imageUrl: string;
+};
+
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const queryParams = {
+      userId: url.searchParams.getAll("userId"),
+      username: url.searchParams.getAll("username"),
+      emailAddress: url.searchParams.getAll("emailAddress"),
+    };
+    const parsedQuery = getUsersSchema.safeParse(queryParams);
+
+    if (!parsedQuery.success) {
+      return new Response(
+        JSON.stringify({ error: parsedQuery.error.flatten() }),
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const userList = await clerkClient.users.getUserList(parsedQuery.data);
+
+    const filteredUserList = userList.data.map((user) => ({
+      id: user.id,
+      name: user.fullName,
+      imageUrl: user.imageUrl,
+    }));
+
+    return new Response(JSON.stringify(filteredUserList));
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+    });
+  }
+}
