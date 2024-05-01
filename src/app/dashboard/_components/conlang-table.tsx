429952abf26767fd1132e5645afeeb59eb49c10d
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-04-30 22:43:11 -0600

    Update styling of conlang table cells

--- src/app/dashboard/_components/conlang-table.tsx
+++ src/app/dashboard/_components/conlang-table.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { type ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 import { useMemo } from "react";
 
+import { CheckCircle } from "~/components/icons/check-circle";
+import { XCircle } from "~/components/icons/x-circle";
 import { DataTable } from "~/components/ui/data-table";
 import { useUsers } from "~/hooks/useUsers";
 
@@ -16,11 +19,9 @@ type Conlang = {
 };
 
 export function ConlangTable(props: { conlangs: Conlang[] }) {
-  const { data: userList } = useUsers({
+  const { data: userList, isLoading } = useUsers({
     userId: props.conlangs.map((conlang) => conlang.ownerId),
   });
-
-  console.log("userList", userList);
 
   const columns: ColumnDef<Conlang>[] = useMemo(
     () => [
@@ -29,23 +30,65 @@ export function ConlangTable(props: { conlangs: Conlang[] }) {
         header: "Name",
       },
       {
-        accessorKey: "ownerId",
-        header: "Owner",
-        cell: ({ row }) =>
-          userList && userList.length > 0
-            ? userList.find((user) => user.id === row.original.ownerId)?.name
-            : "Unknown",
-      },
-      {
         accessorKey: "isPublic",
         header: "Public",
+        cell: ({ row }) => (
+          <div className="flex justify-center">
+            {row.original.isPublic ? (
+              <CheckCircle className="h-6 w-6 text-green-600" />
+            ) : (
+              <XCircle className="h-6 w-6 text-red-800" />
+            )}
+          </div>
+        ),
+      },
+      {
+        accessorKey: "ownerId",
+        header: "Creator",
+        cell: ({ row }) => (
+          <div className="flex items-center gap-2">
+            {isLoading ? (
+              <>
+                <div className="h-6 w-6 rounded-full bg-slate-300" />
+                Loading...
+              </>
+            ) : userList && userList.length > 0 ? (
+              <>
+                <Image
+                  src={
+                    userList.find((user) => user.id === row.original.ownerId)
+                      ?.imageUrl ?? ""
+                  }
+                  alt={
+                    userList.find((user) => user.id === row.original.ownerId)
+                      ?.name + "'s image" ?? "Unknown user"
+                  }
+                  width={24}
+                  height={24}
+                  className="h-6 w-6 rounded-full bg-slate-300"
+                />
+                {
+                  userList.find((user) => user.id === row.original.ownerId)
+                    ?.name
+                }
+              </>
+            ) : (
+              <>
+                <div className="h-6 w-6 rounded-full bg-slate-300" />
+                Unknown
+              </>
+            )}
+          </div>
+        ),
       },
       {
         accessorKey: "createdAt",
         header: "Created",
+        cell: ({ row }) =>
+          new Date(row.original.createdAt).toLocaleDateString(),
       },
     ],
-    [userList],
+    [isLoading, userList],
   );
 
   return (
