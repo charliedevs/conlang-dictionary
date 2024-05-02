93ab3259398721aea6da9709ffe9c175a9af3f39
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-05-01 18:57:02 -0600

    Add pagination to data table component

---
+++ src/components/icons/chevron-double-right.tsx
@@ -0,0 +1,19 @@
+export function ChevronDoubleRight(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+}
