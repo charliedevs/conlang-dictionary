f1c2bbf83497ff1629f61f115e8adfe2077fd263
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-05-11 23:06:05 -0600

    Add breadcrumbs to conlang page

--- src/app/conlang/[name]/page.tsx
+++ src/app/conlang/[name]/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+
 interface ConlangPageProps {
   params: { name: string };
 }
@@ -5,9 +15,27 @@ interface ConlangPageProps {
 export default function ConlangPage({ params }: ConlangPageProps) {
   // TODO: fetch conlang data from database
   return (
-    <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
-      <h1 className="text-center text-2xl font-medium">{params.name}</h1>
-      <p>Description here</p>
+    <div className="flex flex-col p-5">
+      <div className="flex w-full justify-start">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/dashboard">Dashboard</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{params.name}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <div className="container flex flex-col items-center justify-center gap-8 px-4 py-14">
+        <h1 className="text-center text-2xl font-medium">{params.name}</h1>
+        <p>Description here</p>
+      </div>
     </div>
   );
 }
