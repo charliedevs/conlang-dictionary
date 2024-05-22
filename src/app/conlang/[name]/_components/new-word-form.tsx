bb670c8ed2ac26c5e5aa42d067a3eac5ac5fafa4
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-05-21 21:44:44 -0600

    Add Word Form & display words on conlang page

---
+++ src/app/conlang/[name]/_components/new-word-form.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
+import { handleApiErrorResponse } from "~/utils/client-error-handler";
+
+const newWordSchema = z.object({
+  conlangId: z.number(),
+  text: z.string().min(1, "Word text required."),
+  pronunciation: z.string().optional(),
+  gloss: z.string().optional(),
+  definition: z.string().optional(),
+});
+
+export const NewWordForm = (props: { conlangId: number }) => {
+  const form = useForm<z.infer<typeof newWordSchema>>({
+    resolver: zodResolver(newWordSchema),
+    defaultValues: {
+      conlangId: props.conlangId,
+      text: "",
+      pronunciation: "",
+      definition: "",
+    },
+  });
+
+  const router = useRouter();
+  function onSubmit(values: z.infer<typeof newWordSchema>) {
+    fetch("/api/word", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return handleApiErrorResponse(res);
+        }
+        return res.json();
+      })
+      .then(() => {
+        form.reset();
+        router.refresh();
+        toast.success("Word added.");
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        if (err instanceof Error) {
+          toast.error(err.message);
+        } else {
+          toast.error("Failed to add word. Please try again.");
+        }
+      });
+  }
+
+  return (
+    <div id="newWordForm" className="my-10">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex min-w-64 flex-col justify-center space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel>Word</FormLabel>
+                <FormControl>
+                  <Input placeholder="teng" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="pronunciation"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel>Pronunciation</FormLabel>
+                <FormControl>
+                  <Input placeholder="tʃiːn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gloss"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel>Gloss</FormLabel>
+                <FormControl>
+                  <Input placeholder="BOOK" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="definition"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel>Definition</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="book; a written work" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={form.formState.isSubmitting}>
+            Add Word
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
--- src/app/conlang/[name]/page.tsx
+++ src/app/conlang/[name]/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,13 +9,39 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "~/components/ui/breadcrumb";
+import { NewWordForm } from "./_components/new-word-form";
+import { getConlangByName, getWordsByConlangId } from "~/server/queries";
+
+async function WordList(props: { conlangId: number }) {
+  const words = await getWordsByConlangId(props.conlangId);
+  return (
+    <div className="flex flex-col gap-1 p-3">
+      {words.map((word) => (
+        <div
+          key={word.id}
+          className="flex h-8 w-full items-center justify-between"
+        >
+          <div className="flex items-center gap-2">
+            <div className="flex-shrink">{word.text}</div>
+            <div className="flex-shrink">{word.pronunciation}</div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="flex-shrink">{word.gloss}</div>
+            <div className="flex-shrink">{word.definition}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 interface ConlangPageProps {
   params: { name: string };
 }
 
-export default function ConlangPage({ params }: ConlangPageProps) {
+export default async function ConlangPage({ params }: ConlangPageProps) {
   // TODO: fetch conlang data from database
+  const conlang = await getConlangByName(params.name);
   return (
     <div className="flex flex-col p-5">
       <SignedIn>
@@ -37,7 +64,9 @@ export default function ConlangPage({ params }: ConlangPageProps) {
 
       <div className="container flex flex-col items-center justify-center gap-8 px-4 py-14">
         <h1 className="text-center text-2xl font-medium">{params.name}</h1>
-        <p>Description here</p>
+        <p>{conlang.description}</p>
+        <NewWordForm conlangId={conlang.id} />
+        <WordList conlangId={conlang.id} />
       </div>
     </div>
   );
