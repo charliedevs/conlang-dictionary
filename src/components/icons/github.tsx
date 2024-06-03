884abf1b3b30f27c8cbf655e1603fe051e165150
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-06-02 18:06:07 -0600

    Fix error with svg props

--- src/components/icons/github.tsx
+++ src/components/icons/github.tsx
@@ -7,9 +7,9 @@ export function Github(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="size-6"
       {...props}
     >
