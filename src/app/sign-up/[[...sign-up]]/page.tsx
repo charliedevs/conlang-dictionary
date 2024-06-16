64954691ae85d6882b4c85232cf0f1541fe3081d
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-06-16 13:31:54 -0600

    Prevent cut-off on sign-in/sign-up pages

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="mt-8 flex items-center justify-center md:mt-24">
+    <div className="mt-8 flex items-center justify-center py-10 md:mt-24">
       <SignUp />
     </div>
   );
