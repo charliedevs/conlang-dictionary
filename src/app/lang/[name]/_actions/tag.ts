69b5f19801501fd04f4162c3d24c10dc09210a82
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-06-19 13:42:33 -0600

    Only insert new tag if it doesn't exist for any users

--- src/app/lang/[name]/_actions/tag.ts
+++ src/app/lang/[name]/_actions/tag.ts
@@ -1,16 +1,25 @@
 "use server";
-import { addWordTagRelation, insertTag } from "~/server/queries";
+import {
+  addWordTagRelation,
+  getAllWordTags,
+  insertTag,
+} from "~/server/queries";
 import { type Tag } from "~/types/tag";
 
 export async function addTagToWord(wordId: number, tag: Partial<Tag>) {
   let newTag: Tag;
   if (!tag.id) {
     if (!tag.text) throw new Error("Tag text is required");
-    // Add new tag to Tags table first
-    newTag = (await insertTag({
-      text: tag.text,
-      type: "word",
-    })) as Tag;
+    const allTags = await getAllWordTags();
+    const existingTag = allTags.find((t) => t.text === tag.text);
+    if (!existingTag) {
+      newTag = (await insertTag({
+        text: tag.text,
+        type: "word",
+      })) as Tag;
+    } else {
+      newTag = existingTag;
+    }
   } else {
     newTag = tag as Tag;
   }
