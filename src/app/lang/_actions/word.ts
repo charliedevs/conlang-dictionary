9bbb4e997ac4ce0b577661ec1fd6c358ab0241e3
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2025-05-03 17:13:16 -0600

    Remove actions/mutations for old wordSections

--- src/app/lang/_actions/word.ts
+++ src/app/lang/_actions/word.ts
@@ -5,21 +5,10 @@ import {
   type LexicalSectionOrderUpdate,
 } from "~/server/mutations";
 import {
-  deleteDefinition,
   deleteWord,
-  deleteWordSection,
-  insertCustomSection,
-  insertDefinition,
-  insertDefinitionSection,
   insertWord,
-  insertWordSection,
-  updateDefinition,
   updateWord,
-  updateWordSection,
-  type DefinitionInsert,
-  type DefinitionUpdate,
   type WordInsert,
-  type WordSectionUpdate,
   type WordUpdate,
 } from "~/server/queries";
 
@@ -36,71 +25,9 @@ export async function removeWord(wordId: number) {
   await deleteWord(wordId);
 }
 
-// Sections
-export interface CustomSectionCreate {
-  wordId: number;
-  title: string;
-  text: string;
-}
-export async function createCustomSection(cs: CustomSectionCreate) {
-  const newWordSection = await insertWordSection({
-    wordId: cs.wordId,
-    title: cs.title,
-  });
-  const newCustomSection = await insertCustomSection({
-    wordSectionId: newWordSection.id,
-    text: cs.text,
-  });
-  return newCustomSection;
-}
-
-export interface DefinitionSectionCreate {
-  wordId: number;
-  title: string;
-  lexicalCategoryId: number;
-  text: string;
-}
-export async function createDefinitionSection(ds: DefinitionSectionCreate) {
-  const newWordSection = await insertWordSection({
-    wordId: ds.wordId,
-    title: ds.title,
-  });
-  const newDefinitionSection = await insertDefinitionSection({
-    wordSectionId: newWordSection.id,
-    lexicalCategoryId: ds.lexicalCategoryId,
-  });
-  const newDefinition = await insertDefinition({
-    definitionSectionId: newDefinitionSection.id,
-    text: ds.text,
-  });
-  return newDefinition;
-}
-
 export async function updateSectionOrders(
   updates: LexicalSectionOrderUpdate[],
 ) {
   const results = await updateLexicalSectionOrders(updates);
   return results;
-}
-
-export async function removeWordSection(sectionId: number) {
-  await deleteWordSection(sectionId);
-}
-
-export async function addDefinition(d: DefinitionInsert) {
-  const newDefinition = await insertDefinition(d);
-  return newDefinition;
-}
-
-export async function editDefinition(d: DefinitionUpdate) {
-  const definition = await updateDefinition(d);
-  return definition;
-}
-
-export async function removeDefinition(definitionId: number) {
-  await deleteDefinition(definitionId);
-}
-
-export async function editWordSection(section: WordSectionUpdate) {
-  await updateWordSection(section);
 }
