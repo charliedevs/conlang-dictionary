655d4a388dfe7d815f8394c1e6a683386524e6db
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2025-05-05 10:19:37 -0600

    Add Typography extension to tiptap

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,3 +1,4 @@
+import Typography from "@tiptap/extension-typography";
 import { EditorContent, useEditor, type Editor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import {
@@ -62,13 +63,14 @@ export const TextEditor = forwardRef<HTMLDivElement, TextEditorProps>(
             },
           },
         }),
+        Typography,
         Markdown.configure({
           html: true, // Allow HTML input/output
           tightLists: true, // No <p> inside <li> in markdown output
           tightListClass: "tight", // Add class to <ul> allowing you to remove <p> margins when tight
           bulletListMarker: "-", // <li> prefix in markdown output
           linkify: true, // Create links from "https://..." text
-          breaks: false, // New lines (\n) in markdown input are converted to <br>
+          breaks: true, // New lines (\n) in markdown input are converted to <br>
           transformPastedText: true, // Allow to paste markdown text in the editor
           transformCopiedText: false, // Copied text is transformed to markdown
         }),
